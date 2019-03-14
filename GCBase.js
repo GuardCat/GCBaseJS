afb2a0ab19095028acf73e53517907378256f908
GuardCat <storktec@mail.ru>
2019-03-14 20:19:07 +0300

    Наброски в классе

--- GCBase.js
+++ GCBase.js
@@ -1,4 +1,18 @@
 class CGBase {
-	constructor() {
+	constructor(income) {
+		if (!income) throw new Error("GCBase constructor error: no incoming parameters");
+
+		if ( (typeof income) === "string" ) {
+
+			if (income.length <= 30) {
+				this.load(income);
+			} else {
+				this.parse(income);			
+			}
+
+		} else if (typeof income === "object") {
+			if (!income.name || !income.version || !income.description)
+		}
+
 	}
 }
