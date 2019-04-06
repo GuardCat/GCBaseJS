f70fc290a50fc578915402b986883726a0ba7f44
GuardCat <storktec@mail.ru>
2019-04-06 11:47:35 +0300

    добавлены методы: checkAndFixCaption, loadCaptions, модифицирован addTable, некоторые корректировки в документации и примере
    Обнаружены ошибки

--- GCBase.js
+++ GCBase.js
@@ -1,3 +1,7 @@
+/**
+ * База данных
+ * @constructor
+ */
 class GCBase {
 	constructor(income) {
 		if (!income) throw new Error("GCBase constructor error: no incoming parameters");
@@ -17,6 +21,7 @@ class GCBase {
 			
 			if (GCBase.checkDBData(income)) {
 				this.__data = Object.assign({}, income);
+                this.__tables = {};
 			} else if (GCBase.checkDB(income)) {
 				this.__data = Object.assign({}, income.__data);
 				this.__tables = Object.assign({}, income.__tables);
@@ -29,23 +34,79 @@ class GCBase {
 		}
 	}
 	
-	addTable(name,captions) {
+	/*
+     * Добавляем таблицу в экземпляр БД
+     */
+	addTable(name, captions) {
 		if (!name) throw new Error("GCBase addTable: wrong name");
 		if (!GCBase.checkCaptions(captions)) throw new Error("GCBase addTable: wrong captions");
-		this.__tables[name.toString()].__captions = Object.assign({}, captions);
-		this.__tables[name.toString()].__rows = [ ];	
+        name = name.toString( );
+		if (this.__tables[name]) throw new Error("GCBase addTable: table alredy exists");
+        
+        this.__tables[name] = {};
+		this.__tables[name].__captions = this.loadCaptions(captions);
+		this.__tables[name].__rows = [ ];	
 	}
 	
 	table(name) {
         
- }
-	
-	get about() {
+    }
+
+    /*
+     * Прогоняет заголовки через проверку и дополнение, копирует их в новый объект.
+     */
+    loadCaptions(captions) {
+		let result = {}, key;
+		for (key in captions) {
+			result = Object.assign({}, this.checkAndFixCaption(captions[key]));
+		}
+		return result;
+	}
+
+    /* 
+     * Проверяет заголовок таблицы (один конкретный столбец) 
+     * @param {object} caption — заголовок одного столбца
+     * @return {object} дополненная копия заголовка
+     * @throws отсутствие обязательных полей, не проставляемых автоматически, некорректный формат, неизвестный формат
+     */
+    checkAndFixCaption(caption) {
+        if( !(caption && caption instanceof Object && caption.type) ) throw new Error("GCBase addTable: wrong captions");
+        let result = Object.assign({}, caption);
+
+        switch (result.type) {
+            case "text":
+                result.format = "string";
+                break;
+            
+            case "auto":
+                result.format = "integer";
+                result.unique = true;
+                result.next = 0;
+                break;
+            
+            case "link":
+            	if ( !(result.data && result.multiply && result.to)) throw new Error("GCBase addTable: wrong link caption");
+            	break;
+
+			case "date":
+
+
+            default:
+				throw new Error(`GCBase addTable: unknown caption type: ${result.type}`);
+				break;
+		};
+        return result;
+    }
+
+    hasTable
+
+    get about() {
         let obj = {};
         obj.toString = ( ( ) => `${this.__data.name} v${this.__data.version}: ${this.__data.description}` ).bind(this);
 		return Object.assign(obj, this.__data);
 	}
-	
+
+
 	/*
 		Проверяет объект на соответствие формату __data
 	*/
